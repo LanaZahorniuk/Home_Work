8465066e79007aa8b8e45f3703a1a47eb9da81c1
Lana <zaga.svetul@gmail.com>
2023-11-24 11:54:23 +0100

    // Поиск третьего по величине числа в массиве

--- src/main/java/org/example/HW_1/Task.java
+++ src/main/java/org/example/HW_1/Task.java
@@ -4,34 +4,28 @@ import java.util.Arrays;
 
 public class Task {
 
-        public static void main(String[] args) {
-            int [] array = {1,23,-4,56,7,89};
-            System.out.println("массив " + Arrays.toString(array));
-            showArray(array);
-            System.out.println("обратный порядок " + Arrays.toString(array));
+    public static void main(String[] args) {
+        int[] array = {1, 23, -4, 56, 7, 89};
+        System.out.println("массив " + Arrays.toString(array));
+        showArray(array);
+        System.out.println("обратный порядок " + Arrays.toString(array));
+        getThird(array);
+    }
 
-        }
-
-        // Инвертировать массив, то есть изменить порядок элементов в массиве на обратный.
-        public static void showArray (int [] arr) {
-            int ar;
-            for (int i = 0; i < arr.length/2; i++) {
-                ar = arr[i];
-                arr[i] = arr[arr.length - 1 - i];
-                arr[arr.length - 1 - i] = ar;
-            }
-        }
-
-        // Поиск третьего по величине числа в массиве
-        public static void getThird (int[] arr) {
-            int num1;
-            int num2;
-            for (int i = 0; i < arr.length; i++) {
-                num1 = arr[i];
-                if (arr[i]>num1) {
-                    num2 = arr[i];
-                }
-            }
+    // Инвертировать массив, то есть изменить порядок элементов в массиве на обратный.
+    public static void showArray(int[] arr) {
+        int ar;
+        for (int i = 0; i < arr.length / 2; i++) {
+            ar = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = ar;
         }
     }
+
+    // Поиск третьего по величине числа в массиве
+    public static void getThird(int[] arr) {
+        Arrays.sort(arr);
+        System.out.println("третье по величине число в массиве: " + arr[2]);
+    }
+}
 
